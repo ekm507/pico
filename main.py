a458084ec245fcf639136d9e0478bf03a8d528b6
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-04-18 15:38:58 +0430

    experiment success!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import subprocess
 
 HOST = '0.0.0.0'
 PORT = 80
@@ -24,15 +25,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(f'request is:\n {text}')
             method = text.split(' ')[0]
             link = text.split(' ')[1]
+            host = text.split('\n')[1]
             if method == 'GET':
                 if link == '/':
                     filename = PATH + '/index.html'
+                elif link == '/word':
+                    cmd = ['python', f'{PATH}/back/words.py', '6', 'time']
+                    subprocess.Popen(cmd).wait()
+                    filename = PATH + '/back/words-out.html'
+                    header = 'HTTP/1.0 200 OK\nContent-Type: text/html; charset=utf-8\nLanguage: fa\n\n'.encode('utf-8')
+                    content = open(filename, 'rb').read()
+                    print(content)
                 else:
                     filename = PATH + link.split('?')[0]
-                print(filename)
+                    print(filename)
                 try:
                     header = 'HTTP/1.0 200 OK\nContent-Type: text/html\n\n'.encode('utf-8')
-                    content = open(filename).read().encode('utf-8')
+                    content = open(filename, 'rb').read()
                 except FileNotFoundError:
                     header = 'HTTP/1.0 404 Not Found\n\n'.encode('utf-8')
                     content = open(PATH+'/errors/404.html').read().encode('utf-8')
