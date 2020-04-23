adffb2c89800a91a2b00310a79dda1ca6ffe5ddd
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-04-24 01:31:09 +0430

    object

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-import socket
-import threading
-import _thread
-from hooks import eprint
-import time
-
-
-def parse_request(data:bytes):
-    time.sleep(1)
-    eprint(data)
-
-def req_handler(conn:socket.socket):
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            eprint('END')
-            break
-        
-        parse_request(data)
-
-    conn.close()
+from webserver import webserver
+import sys
 
 def main():
-    HOST = '0.0.0.0'
-    PORT = 1234
+    try:
+        HOST = '0.0.0.0'
+        PORT = 1234
+        address = (HOST, PORT)
+        ws = webserver(address, debug=True)
+        ws.start_receiving()
+    except KeyboardInterrupt:
+        del ws
+        sys.exit(" Keyboard Interrupt Exit(ctrl+c)")
+        
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-
-    s.listen(100)
-    eprint(f'started listening on port {PORT}')
-
-    while True:
-        conn, address = s.accept()
-
-        eprint(f'connected to {address[0]} : {address[1]}')
-
-        _thread.start_new_thread(req_handler, (conn,))
-
-    s.close()
 
 if __name__ == "__main__":
     main()
