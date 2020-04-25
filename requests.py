3be6d17f04c22189345ec05c7772886d0c1f0c96
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-04-25 06:34:15 +0430

    parametric API handler

--- requests.py
+++ requests.py
@@ -11,58 +11,87 @@ class request_handler:
         self.debug = debug
         self.bufferSize = 1024
         self.path = path
+
+        self.backs = list()
+        backs = open('backs.conf', 'r').read()
+        for line in backs.split('\n'):
+            if line[0] == '#':
+                pass
+            else:
+                link = line.split(' ')[0]
+                outfilename = line.split(' ')[1]
+                runnable = line[len(link) + len(outfilename) + 2 :]
+                self.backs.append((link, outfilename, runnable))
+        
     
     # you can create a thread of this function for each client
     # main request handler function
     def handle(self, conn:socket.socket, name=''):
         # start communicating
-        while True:
-            # wait for receiving new data
-            data = conn.recv(self.bufferSize)
-            # check if connection is going to end
-            if not data:
+        try:
+            while True:
+                # wait for receiving new data
+                data = conn.recv(self.bufferSize)
+                # check if connection is going to end
+                if not data:
+                    if self.debug == True:
+                        eprint(name, ':', '__END__')
+                    # end the connection
+                    break
+                # now the request is going to get handled
+
+
+                self.default_answer(conn, data, name=name)
+
                 if self.debug == True:
-                    eprint(name, ':', '__END__')
-                # end the connection
-                break
-            # now the request is going to get handled
-
-
-            self.default_answer(conn, data, name=name)
-
-            if self.debug == True:
-                eprint(name, ': request is:\n', str(data, encoding='utf-8'))
-            
-            conn.shutdown(1)
-
-        conn.close()
+                    eprint(name, ': request is:\n', str(data, encoding='utf-8'))
+                
+                conn.shutdown(1)
+            conn.close()
+        except KeyboardInterrupt:
+            conn.close()
             
 
     def default_answer(self, conn, data, name=''):
         text = str(data, 'utf-8')
         method = text.split(' ')[0]
         link = text.split(' ')[1]
+        name = link.split('?')[0]
+        params = link.split('?')[1:]
         host = text.split('\n')[1]
+        data = b''
         if method == 'GET':
-            if link == '/':
-                filename = self.path + '/index.html'
-            elif link == '/word':
-                cmd = ['python', f'{self.path}/back/words.py', '6', 'time']
-                subprocess.Popen(cmd).wait()
-                filename = self.path + '/back/words-out.html'
-                header = 'HTTP/1.0 200 OK\nContent-Type: text/html; charset=utf-8\nLanguage: fa\n\n'.encode('utf-8')
-                content = open(filename, 'rb').read()
-                print(content)
-            else:
-                filename = self.path + link.split('?')[0]
-                print(filename)
-            try:
+            status = 0
+            for a, b, c in self.backs:
+                if name == a:
+                    cmd = c.split(' ') + params
+                    print(cmd)
+                    subprocess.Popen(cmd).wait()
+                    f = open(self.path + b, 'r')
+                    data = f.read().encode('utf-8')
+                    f.close()
+                    status = 200
+            if status == 0:
+                if name == '/':
+                    filename = self.path + '/index.html'
+                    status = 200
+                else:
+                    filename = self.path + link.split('?')[0]
+                    status = 200
+                try:
+                    data = open(filename, 'rb').read()
+                    status = 200
+                except FileNotFoundError:
+                    status = 404
+           
+            if status == 200:
                 header = 'HTTP/1.0 200 OK\nContent-Type: text/html\n\n'.encode('utf-8')
-                content = open(filename, 'rb').read()
-            except FileNotFoundError:
+            elif status == 404:
                 header = 'HTTP/1.0 404 Not Found\n\n'.encode('utf-8')
-                content = open(self.path+'/errors/404.html').read().encode('utf-8')
-            texttosend = header + content
+                data = open(self.path + '/errors/404.html', 'rb').read()
+            else:
+                pass
+            texttosend = header + data
             conn.send(texttosend)
 
     # nothing yet.
