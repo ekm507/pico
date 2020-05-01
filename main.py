ca009e567a798186cf3db50e38fa00eca1db39d5
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-05-01 15:38:05 +0430

    get port number from shell

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from webserver import webserver
 # basic system functions
 import sys
 import os
+
 # main function
 def main():
     # this is for closing socket when errors occur
     try:
         # set listening host and port
         HOST = '0.0.0.0'
-        PORT = 1234
+        # try getting port number from shell
+        if len(sys.argv) > 1:
+            PORT = int(sys.argv[1])
+        # if not provided by shell, default port number is this
+        else:
+            PORT = 1234
         # set listening address tuple
         address = (HOST, PORT)
         # create a new webserver object. with log mode set as true
